9cdd674f0cf6f2144779a9fb0d90f9b2e92fbd09
DariaKostrik <dolorez0927@yandex.ru>
2023-08-07 20:57:50 +0300

    add smaller conteiner for mobile to scrolling

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const marginValue=250;
-let currentBlock=0;
-const firstCard=document.getElementById().querySelector('.category-card:first-child') 
-
-// const container = document.querySelector('.container-with-goods');
-
-function leafSlide(direction){
-currentBlock+=direction;
-    firstCard.style.marginLeft =-marginValue*currentBlock +'px';
-// if(currentBlock>4){
-//     currentBlock=0;
-//     }
+let currentBlock = 0;
+const firstCard = document.querySelector('.plant-in-line:first-child');
+const parent = document.querySelector('.container-for-plant-in-line');
+const allStyle =window.getComputedStyle(parent);
+const gap = allStyle.gap;
+let gapN = parseInt(gap);
+let margN = parseInt(firstCard.clientWidth);
+console.log(gapN);
+let marginValue = margN+gapN;
+console.log(marginValue);
+// const marginValue=10;
+function leafSlide(direction) {
+    currentBlock += direction;
+    if(currentBlock>7 || currentBlock<-1 )
+    {
+        currentBlock=0;
+    }
+    firstCard.style.marginLeft = -marginValue * currentBlock + 'px';
 }
-
 
